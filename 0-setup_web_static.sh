7c36b9a7e34eda1b36029ad2118e80a2ed8dfe45
Linda-Njau <lindanjau21@.com>
2023-04-09 21:46:50 +0300

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,6 +11,6 @@ sudo mkdir -p data/web_static/shared/
 sudo chown -R ubuntu:ubuntu /data/
 echo "<html><head><title>Test</title></head><body>This is a test page</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 sudo rm -rf /data/web_static/current
-sudp ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo sed -i '/server_name localhost;/ a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx start
