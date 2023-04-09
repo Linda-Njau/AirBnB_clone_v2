529ebdb87abd65bdcbc84ccff117425ea6908c49
Linda-Njau <lindanjau21@.com>
2023-04-10 01:26:26 +0300

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 #this script sets up web servers for deployment
 
-if ! command -v nginx &> /dev/null
-then
-    sudo apt-get update
-    sudo apt-get install -y nginx
-fi
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p data/web_static/shared/
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+sudo echo "Fake text to test" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-echo "<html><head><title>Test</title></head><body>This is a test page</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-sudo rm -rf /data/web_static/current
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-sudo sed -i '/server_name localhost;/ a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx start
